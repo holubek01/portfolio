a96a2c340c92529af721750fae1b4af8b3281d85
holubek01 <261718@student.pwr.edu.pl>
2023-04-26 00:32:57 +0200

    change file name

--- menu.js
+++ menu.js
@@ -1,145 +1,14 @@
 /*jslint browser:true */
 /*global document */
-/*
 
 const menuToggle = document.querySelector(".menu-toggle");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
 const navLinks = document.querySelectorAll(".nav-item a");
+const header = document.querySelector('header'); 
 
-let showMenu = false;
-menuToggle.addEventListener("click", toggleMenu);
-
-
-
-
-const currentPage = window.location.pathname.split("/").pop().replace(".html", "").replace("_"," "); // get the current page name
-console.log(currentPage);
-
-navLinks.forEach(function(link) {
-    if (link.getAttribute("href") === currentPage) {
-        link.parentElement.classList.add("active"); // add "active" class to the link's parent if it matches the current page
-    }
-    
-    link.addEventListener("click", function () {
-        // remove "active" class from all links
-        navItems.forEach((navItem) => navItem.classList.remove("active"));
-
-        // add "active" class to the clicked link's parent
-        link.parentElement.classList.add("active");
-    });
-
-    link.addEventListener("mouseover", function () {
-        link.style.textShadow = "2px 2px 4px rgba(0,0,0,0.9)";
-        link.style.color = "black";
-    });
-    
-    link.addEventListener("mouseout", function () {
-        link.style.textShadow = "none";
-        link.style.color = "white";
-    });
-});
-
-//dla mniejszych ekranów
-function toggleMenuSmall() {
-    if (window.innerWidth <= 768) {
-        menuToggle.classList.remove("hidden");
-        menuToggle.addEventListener("click", toggleMenu);
-    } else {
-        menuToggle.classList.add("hidden");
-        menuToggle.removeEventListener("click", toggleMenu);
-        menuNav.classList.remove("show");
-        navItems.forEach((item) => item.classList.remove("show"));
-        showMenu = false;
-    }
-}
-
-window.addEventListener("resize", toggleMenuSmall);
-
-function toggleMenu() {
-    if (!showMenu) {
-        menuToggle.classList.add("close");
-        menuNav.classList.add("show");
-        navItems.forEach((item) => item.classList.add("show"));
-
-        showMenu = true;
-
-    } else {
-        menuToggle.classList.remove("close");
-        menuNav.classList.remove("show");
-        navItems.forEach((item) => item.classList.remove("show"));
-
-        showMenu = false;
-    }
-
-    // hide all other links except the active one
-    const activeLink = document.querySelector(".nav-item.active");
-    navItems.forEach((item) => {
-        console.log(activeLink);
-        console.log(item);
-        if (item !== activeLink) {
-            item.classList.remove("show");
-        }
-    });
-
-    
-
-
-}
-*/
-
-
-
-
-
-
-/*
-function toggleMenu() {
-    if (!showMenu) {
-        menuToggle.classList.add("close");
-        menuNav.classList.add("show");
-        navItems.forEach((item) => item.classList.add("show"));
-
-        // hide all other links except the active one
-        const activeLink = document.querySelector(".nav-item.active");
-        navItems.forEach((item) => {
-            if (item !== activeLink) {
-                item.classList.remove("show");
-            }
-        });
-
-        showMenu = true;
-
-    } else {
-        menuToggle.classList.remove("close");
-        menuNav.classList.remove("show");
-        navItems.forEach((item) => item.classList.remove("show"));
-
-        // remove "show" class from all navItems except the active one
-        const activeLink = document.querySelector(".nav-item.active");
-        navItems.forEach((item) => {
-            if (item !== activeLink) {
-                item.classList.remove("show");
-            }
-        });
-
-        showMenu = false;
-    }
-}
-*/
-
-
-/*jslint browser:true */
-/*global document */
-
-const menuToggle = document.querySelector(".menu-toggle");
-const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".nav-item");
-const navLinks = document.querySelectorAll(".nav-item a");
-
-const header = document.querySelector('header'); // pobierz element header
-
-header.classList.remove('no-js'); // usuń klasę 'no-js' z elementu header
+header.classList.remove('no-js'); 
+document.documentElement.classList.add('js');
 
 
 let showMenu = false;
@@ -147,10 +16,6 @@ let showMenu = false;
 menuToggle.addEventListener("click", toggleMenu);
 window.addEventListener("load", toggleMenuSmall);
 window.addEventListener("resize", toggleMenuSmall);
-
-document.documentElement.classList.add('js');
-
-//const navbar = document.querySelector(".menu-nav");
 
 
 function toggleMenu() {
@@ -201,7 +66,6 @@ function toggleMenu() {
 
 
 function toggleMenuSmall() {
-    console.log("tomekl");
     if (window.innerWidth <= 768) {
         menuToggle.classList.remove("hidden");
         menuToggle.addEventListener("click", toggleMenu);
@@ -217,8 +81,6 @@ function toggleMenuSmall() {
     const currentPagee = window.location.pathname.split("/").pop().replace("_"," "); // get the current page name
 
 navLinks.forEach(function(link) {
-        console.log(link.getAttribute("href").replace("./", ""));
-        console.log(currentPagee);
     if (link.getAttribute("href").replace("./", "").replace("_"," ") === currentPagee) {
         link.parentElement.classList.add("active"); // add "active" class to the link's parent if it matches the current page
     }
@@ -233,11 +95,8 @@ const li = ul.querySelectorAll('li');
 const middleIndex = Math.floor(li.length / 2);
 const activeIndex = Array.from(li).indexOf(activeLi);
 
-console.log(activeLi);
-console.log(li[middleIndex]);
 
 if (li.length > 1) {
-    console.log("chuj");
     const firstLi = li[middleIndex];
     const thirdLi = activeLi;
     let secondLi;
@@ -268,27 +127,22 @@ if (li.length > 1) {
     
 }}
 
-const currentPage = window.location.pathname.split("/").pop().replace("_"," "); // get the current page name
+
+//pobierz aktualną stronę
+//const currentPage = window.location.pathname.split("/").pop().replace("_"," "); 
 
 navLinks.forEach(function(link) {
-    /*
-        console.log(link.getAttribute("href").replace("./", ""));
-        console.log(currentPage);
-    if (link.getAttribute("href").replace("./", "") === currentPage) {
-        link.parentElement.classList.add("active"); // add "active" class to the link's parent if it matches the current page
-    }
-    */
     
     link.addEventListener("click", function (event) {
         if (showMenu) {
-            // remove "active" class from all links
+            // usuwa klase 'active' ze wszystkich linków i dodaje ją tylko do aktualnie klikniętego
             navItems.forEach((navItem) => navItem.classList.remove("active"));
-    
-            // add "active" class to the clicked link's parent
             event.target.parentElement.classList.add("active");
 
         }
     });
+
+
 
     link.addEventListener("mouseover", function () {
         link.style.textShadow = "2px 2px 4px rgba(0,0,0,0.9)";
